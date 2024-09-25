3488be21ead766f4ee4876d3603717431efe68f1
AntonioJanael <66491183+AntonioJanael@users.noreply.github.com>
2024-09-24 21:55:53 -0300

    update jump game

--- leet_code/jump_game.py
+++ leet_code/jump_game.py
@@ -2,20 +2,16 @@ from typing import List
 
 
 def can_jump(nums: List[int]) -> bool:
-    if nums[0] == 0 and len(nums) == 1:
-        return True
-    elif nums[0] == 0:
-        return False
-    index = 0
-    while index < len(nums):
-        maximum_jump_length = nums[index]
-        next_index = index + maximum_jump_length
-        if next_index == len(nums) - 1:
-            return True
-        while next_index < len(nums) - 1 and nums[next_index] == 0 and maximum_jump_length > 0:
-            maximum_jump_length -= 1
-            next_index = index + maximum_jump_length
-        if maximum_jump_length == 0:
-            return False
-        index += next_index
-    return True
+    length = len(nums) - 1
+    count = length
+    last_reachable = length
+    reachable = False
+    while count >= 0:
+        if count + nums[count] >= last_reachable:
+            reachable = True
+            last_reachable = count
+        else:
+            reachable = False
+        count -= 1
+    return reachable
+
